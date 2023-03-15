62d425681b7280ab43ba0e17a897ce20f6e682a8
Habib Maksoud <habibmaksoud@gmail.com>
2023-03-15 16:18:56 -0700

    updated the seeds

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,7 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -17,25 +16,11 @@ CREATE TABLE department (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE employee_department (
-    employee_id INT NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY (employee_id, department_id),
-    FOREIGN KEY (employee_id) REFERENCES employee(id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
-);
-
-CREATE TABLE employee_role (
-    employee_id INT NOT NULL,
-    role_id INT NOT NULL,
-    PRIMARY KEY (employee_id, role_id),
-    FOREIGN KEY (employee_id) REFERENCES employee(id),
-    FOREIGN KEY (role_id) REFERENCES role(id)
-);
-
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
     PRIMARY KEY (id)
 );
 
---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('John', 'Doe', 1),
+       ('Habib', 'Khan', 2),
+       ('Sally', 'Smith', 3),
+       ('Bob', 'Jones', 4),
+       ('Mary', 'Johnson', 5),
+       ('Sue', 'Williams', 6);
+
+INSERT INTO department (name)
+VALUES ('Sales'),
+       ('Engineering'),
+       ('Finance'),
+       ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 100000, 1),
+       ('Salesperson', 80000, 1),
+       ('Lead Engineer', 110000, 2),
+       ('Software Engineer', 100000, 2),
+       ('Accountant', 125000, 3),
+       ('Legal Team Lead', 130000, 4),
+       ('Lawyer', 120000, 4);
