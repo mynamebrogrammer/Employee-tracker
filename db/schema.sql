a26af3932fe79d2382506120ba2a2b1c9d38dce1
Habib Maksoud <habibmaksoud@gmail.com>
2023-03-16 16:00:50 -0700

    again updated prompts and all should either be viewed or updated upon user input

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,26 @@ USE employee_db;
 
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
-    dept_name VARCHAR(255) NOT NULL,
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    foreign key (department_id) references departments(id)
 );
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    foreign key (role_id) references roles(id),
+    foreign key (manager_id) references employees(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,26 @@
-INSERT INTO departments (dept_name)
-VALUES ('Sales'),
-       ('Engineering'),
-       ('Finance'),
-       ('Legal');
+INSERT INTO departments (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ('Sales Lead', 100000, 1),
-       ('Salesperson', 80000, 1),
-       ('Lead Engineer', 110000, 2),
-       ('Software Engineer', 100000, 2),
-       ('Accountant', 125000, 3),
-       ('Legal Team Lead', 130000, 4),
-       ('Lawyer', 120000, 4);
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 200000, 2),
+    ('Software Engineer', 180000, 2),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 200000, 4),
+    ('Lawyer', 180000, 4);
 
-INSERT INTO employees (first_name, last_name, role_id)
-VALUES ('John', 'Doe', 1),
-       ('Habib', 'Khan', 2),
-       ('Sally', 'Smith', 3),
-       ('Bob', 'Jones', 4),
-       ('Mary', 'Johnson', 5),
-       ('Sue', 'Williams', 6);
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Malia', 'Brown', 5, NULL),
+    ('Sarah', 'Lourd', 6, NULL),
+    ('Tom', 'Allen', 7, 6);
